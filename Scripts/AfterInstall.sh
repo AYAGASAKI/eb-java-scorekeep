52d2c45a5448ef9ff98de23901d6d51277728844
EC2 Default User <ec2-user@ip-172-31-27-198.us-east-2.compute.internal>
2019-03-13 02:38:16 +0000

    test1

--- Scripts/AfterInstall.sh
+++ Scripts/AfterInstall.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-echo "start"
+sudo mkdir -p /home/ec2-user/build/libs/
+sudo cp -f /opt/codedeploy/build/libs/scorekeep-api-1.0.0.jar /home/ec2-user/build/libs/
+sudo chown -R ec2-user:ec2-user /home/ec2-user/
 
